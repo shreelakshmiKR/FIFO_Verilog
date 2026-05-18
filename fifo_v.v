d4a6e0eca84e19d22648435241fc192cfaf9828c
Shreelakshmi K R <151436236+shreelakshmiKR@users.noreply.github.com>
2026-05-18 16:44:20 +0530

    Update fifo_v.v

--- fifo_v.v
+++ fifo_v.v
@@ -1,65 +1,98 @@
-module fifo_sync
-    // Parameters section
-    #( parameter FIFO_DEPTH = 8,
-	   parameter DATA_WIDTH = 32)
-    // Ports section   
-	(input clk, 
-     input rst_n,
-     input cs,     // chip select	 
-     input wr_en, 
-     input rd_en, 
-     input [DATA_WIDTH-1:0] data_in, 
-     output reg [DATA_WIDTH-1:0] data_out, 
-	 output empty,
-	 output full); 
-
-  localparam FIFO_DEPTH_LOG = $clog2(FIFO_DEPTH);// 
+`timescale 1ns/1ns
+module tb_fifo_sync();
 	
-    // Declare a by-dimensional array to store the data
-  reg [DATA_WIDTH-1:0] fifo [0:FIFO_DEPTH-1];// depth 8 => [0:7] 32 bit elements
+	// Testbench variables
+	parameter FIFO_DEPTH = 8;
+	parameter DATA_WIDTH = 32;
+    reg clk = 0; 
+    reg rst_n;
+    reg cs;	 
+    reg wr_en;
+    reg rd_en;
+    reg [DATA_WIDTH-1:0] data_in;
+    wire [DATA_WIDTH-1:0] data_out;
+	wire empty;
+	wire full;
 	
-	// Wr/Rd pointer have 1 extra bits at MSB
-  reg [FIFO_DEPTH_LOG:0] write_pointer;//3:0
-  reg [FIFO_DEPTH_LOG:0] read_pointer;//3:0
+    integer i;
+	
+	// Instantiate the DUT
+	fifo_sync 
+	    #(.FIFO_DEPTH(FIFO_DEPTH),
+          .DATA_WIDTH(DATA_WIDTH))
+        dut
+  	    (.clk     (clk     ), 
+         .rst_n   (rst_n   ),
+         .cs      (cs      ),	 
+         .wr_en   (wr_en   ), 
+         .rd_en   (rd_en   ), 
+         .data_in (data_in ), 
+         .data_out(data_out), 
+	     .empty   (empty   ),
+	     .full    (full    ));
 
-  //write
-    always @(posedge clk or negedge rst_n) 
-      begin
-      if(!rst_n)//rst =0 system reset happens
-		    write_pointer <= 0;
-      else if (cs && wr_en && !full) begin
-          fifo[write_pointer[FIFO_DEPTH_LOG-1:0]] <= data_in;
-	       write_pointer <= write_pointer + 1'b1;
-      end
-      end
+  	// Create the clock signal
+	always begin #5 clk = ~clk; end
   
-	//read
-	always @(posedge clk or negedge rst_n) 
-      begin
-	    if(!rst_n)
-		    read_pointer <= 0;
-      else if (cs && rd_en && !empty) begin
-          	data_out <= fifo[read_pointer[FIFO_DEPTH_LOG-1:0]];
-	        read_pointer <= read_pointer + 1'b1;
-      end
-	end
+    task write_data(input [DATA_WIDTH-1:0] d_in);
+	    begin
+		    @(posedge clk); // sync to positive edge of clock
+			cs = 1; wr_en = 1;
+			data_in = d_in;
+			$display($time, " write_data data_in = %0d", data_in);
+			@(posedge clk);
+		    cs = 1; wr_en = 0;
+		end
+	endtask
 	
-	// Declare the empty/full logic
-    assign empty = (read_pointer == write_pointer);
-	assign full  = (read_pointer == {~write_pointer[FIFO_DEPTH_LOG], write_pointer[FIFO_DEPTH_LOG-1:0]});
+	task read_data();
+	    begin
+		    @(posedge clk);  // sync to positive edge of clock
+			cs = 1; rd_en = 1;
+			@(posedge clk);
+			//#1;
+		    $display($time, " read_data data_out = %0d", data_out);
+		    cs = 1; rd_en = 0;
+		end
+	endtask
+	
 
-  
-  /*
-    always @(posedge clk) begin
-	    if (cs && wr_en && !full)
-	        fifo[write_pointer[FIFO_DEPTH_LOG-1:0]] <= data_in;
-	end
 	
-	always @(posedge clk or negedge rst_n) begin
-	    if (!rst_n)
-		    data_out <= 0;
-		else if (cs && rd_en && !empty)
-	        data_out <= fifo[read_pointer[FIFO_DEPTH_LOG-1:0]];
+    // Create stimulus	  
+    initial begin
+	    #1; 
+		rst_n = 0; rd_en = 0; wr_en = 0;
+		
+        @(posedge clk) 
+		rst_n = 1;
+		$display($time, "\n SCENARIO 1");
+		write_data(1);
+		write_data(10);
+		write_data(100);
+		read_data();
+		read_data();
+		read_data();
+		//read_data();
+		
+        $display($time, "\n SCENARIO 2");
+		for (i=0; i<FIFO_DEPTH; i=i+1) begin
+		    write_data(2**i);
+			read_data();        
+		end
+
+        $display($time, "\n SCENARIO 3");		
+		for (i=0; i<=FIFO_DEPTH; i=i+1) begin
+		    write_data(2**i);
+		end
+		
+		for (i=0; i<FIFO_DEPTH; i=i+1) begin
+			read_data();
+		end
+		
+	    #40 $finish;
 	end
-*/
+  
+  initial begin
+    $dumpfile("dump.vcd"); $dumpvars;
+  end
 endmodule
